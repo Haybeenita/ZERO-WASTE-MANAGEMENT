86cb013b90ad201bbecfecb1cfe97f334ad03b06
cynthia Amarachi <ceeyntia@gmail.com>
2024-03-17 13:11:16 -0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Home from './pages/Home-page'
 import './App.css'
 function App() {
-
+  useEffect(() => {
+    AOS.init({
+      // Global settings
+      once:false
+    });
+  }, []);
   return (
     <>
+     <div className='inner-width'>
       <Home/>
+     </div>
     </>
   )
 }
 
 export default App
+
+
+
