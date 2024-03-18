9e6ed2e8c879c69ecec1c77a898b089545bb0292
cynthia Amarachi <ceeyntia@gmail.com>
2024-03-18 11:55:40 -0700

    created input component

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,7 +1,9 @@
 import styles from "./button.module.css"
-const Button =({variant,size,children})=>{
+const Button =({variant,size,padding,children})=>{
     return <div>
-        <button data-variant={variant} data-size={size} className={styles.btn} >{children}</button>
+        <button data-variant={variant} data-size={size}  className={styles.btn} >{children}</button>
     </div>
 }
 export default Button;
+
+//style={{padding: padding ? padding: '20px'}}
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,9 @@
 import styles from './input.module.css'
-const Input = ({size})=>{
-    return <div>
-        <input placeholder ='Enter your email' data-size={size}/>
+const Input = ({label,placeholder,name,value,size,variant,fullWidth =true,type,padding})=>{
+    return <div className={styles.inputWrapper}>
+        <label >{label}</label>
+        <input data-size={size} style={{padding:padding? padding:'21px 0 21px 18px'}} data-variant={variant}data-fullWidth={fullWidth} value={value} name={name} type={type} placeholder={placeholder} className={styles.input}/>
     </div>
 }
 export default Input;
+
