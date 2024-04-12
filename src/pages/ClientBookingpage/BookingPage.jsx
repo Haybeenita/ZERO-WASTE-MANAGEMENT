99ab6f7a8af46017add5eeb7d2d93c44347d5034
cynthia Amarachi <ceeyntia@gmail.com>
2024-04-11 19:14:01 -0700

    added booking page

--- src/pages/ClientBookingpage/BookingPage.jsx
+++ src/pages/ClientBookingpage/BookingPage.jsx
@@ -4,54 +4,61 @@ import Button from '../../components/Buttons'
 
 const BookingPage =()=> {
   return (
-    <div>
-         <div className="relative h-screen bg-cover bg-center filter bg-[url('../../../src/assets/images/animation1.jpg')]">
-      {/* Background Image */}
-      <div className="absolute inset-0 z-0 bg-black/40"></div>
-
-      <div className="absolute inset-0 flex justify-center items-center z-10">
-        <div className="w-[30rem] h-[40rem] pt-[2.5rem] mt-[0.75rem] shadow-2xl bg-white/35 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg">
-          <div className="shadow-2xl w-[30rem] h-[40rem]">
+    <div className='flex justify-between w-full '>
+      <div className=" max-w-[45rem] object-cover ">
+        <img  src='public/Bookingpage 1.png'/>
+      </div>
+      {/* <div className=""> </div> */}
+      <div className=" flex">
+        <div className="w-[40rem] mr-[4rem] h-[40rem] pt-[2.5rem] mt-[0.75rem] ">
+          <div className=" w-[40rem] h-[40rem] flex flex-col items-center  pt-[1rem] bg-[#f0f0f0]  ">
             <h1 className="flex items-center justify-center font-bold text-2xl text-black mb-2">
               Booking Details
             </h1>
-            <hr className="w-[25rem] border-black flex items-center justify-center m-auto"></hr>
-            <p className="flex font-semibold text-black items-center justify-center gap-1 mt-[1rem] text-lg">
-              Create an
-              <span className="text-[#0E5808] font-medium"> Account </span> to
-              access features
-            </p>
-            <div className="flex flex-col w-[20rem] ml-[4rem] mt-[2rem]">
+            <hr className="w-[25rem] my-[1rem] border-black"></hr>
+            <div className="flex flex-col w-[30rem]">
               <div>
-                <p className="text-black font-medium">Profile Name:</p>
-
-                <Input variant="primary" padding="8px" placeholder="name" />
-                <p className="text-black font-medium">Email:</p>
-                <i
-                  src="/emailogoregister.png"
-                  alt=""
-                  className="absolute w-4 h-4 mt-4 ml-2"
-                />
-                <Input variant="primary" padding="8px" placeholder="@" />
-                <p className="text-black font-medium">Phone Number:</p>
-                <Input variant="primary" padding="8px" placeholder="+234" />
-                <p className="text-black font-medium">Password:</p>
-                <Input variant="primary" padding="8px" placeholder="*******" />
+                <div className='flex justify-between'>
+                  <label className="text-black font-medium mr-[1.5rem] text-[16px]">Profile Name
+                    <Input className='bg-[#dbe6da]' variant="primary" padding="8px" placeholder="name" />
+                  </label>
+                  <label className="text-black font-medium text-[16px]">Last Name
+                    <Input variant="primary" padding="8px" placeholder="name" />
+                  </label>
+                </div>
+                <div className='flex justify-between'>
+                  <label className="text-black font-medium mr-[1.5rem] text-[16px]">Email
+                    <Input variant="primary" padding="8px" placeholder="@" />
+                  </label>
+                  <label className="text-black font-medium text-[16px]">Phone Number
+                    <Input variant="primary" padding="8px" placeholder="+234" />
+                  </label>
+                </div>
+                <p className="text-black font-medium">Address</p>
+                <Input variant="primary" padding="8px" placeholder="Address" /> 
+               <div className='flex items-center '>
+                  <label className="text-black font-medium w-[10rem] text-[16px] mr-[2rem]">PickUp Date
+                    <Input className="placeholder-text-[13px] px-[10px]" variant="primary" padding="6px" placeholder="DD/MM/YY" />
+                  </label>
+                  <select className="select w-full max-w-xs bg-[#f6eeee]">
+                    <option disabled selected>Type of Waste</option>
+                    <option>Homer</option>
+                    <option>Marge</option>
+                    <option>Bart</option>
+                    <option>Lisa</option>
+                    <option>Maggie</option>
+                  </select> 
+               </div>
               </div>
-
-              <div className="flex justify-center mt-4">
+              <div className="flex justify-center mt-4 font-bold">
                 <Button variant="primary" size="large">
-                  Register
+                 Confirm Booking
                 </Button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-        
-      
-        
+      </div>    
     </div>
   )
 }
