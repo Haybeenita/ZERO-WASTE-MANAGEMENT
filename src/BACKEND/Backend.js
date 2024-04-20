1bce7506288e0cf7b20a7f19831fb56be0ce37f5
Miss shelby <ceeyntia@gmail.com>
2024-04-20 18:32:48 +0100

    worked on booking page

--- src/BACKEND/Backend.js
+++ src/BACKEND/Backend.js
@@ -4,4 +4,4 @@ export const RegisterApi = `http://37.27.82.158:8000/api/v1/users/register`;
 export const LoginApi =`http://37.27.82.158:8000/api/v1/login`
 export const booking =`http://37.27.82.158:8000/api/v1/booking/booking`
 export const profile =`http://37.27.82.158:8000/api/v1/users/profile`
-//   'https://5edc-105-112-208-196.ngrok-free.app/api/v1/users/profile'
+export const Bookingsorder =`http://37.27.82.158:8000/api/v1/booking/bookings`
